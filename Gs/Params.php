5cc1c51aeae1967854b73b50add6ad8f52448c14
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-01-13 10:12:38 -0200

    Created Gs_Params for easy params handling

---
+++ Gs/Params.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * @author Marcelo Jacobus <marcelo.jacobus@gmail.com>
+ */
+class Gs_Params implements Iterator, ArrayAccess
+{
+
+    /**
+     * @var array
+     **/
+    protected $_params = array();
+
+    /**
+     * @param array $params Key => Value array
+     */
+    public function __construct(array $params = array())
+    {
+        $this->setValues($params);
+    }
+
+    /**
+     * Set the params
+     *
+     * @param array $params the params to be set
+     * @return Gs_Params
+     */
+    public function setValues(array $params = array())
+    {
+        $this->_params = $params;
+        return $this;
+    }
+
+    /**
+     * Set the value
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return Gs_Params
+     * */
+    public function setValue($key, $value)
+    {
+        return $this->_params[$key] = $value;
+        return $this;
+    }
+
+    /**
+     * Get the param by key
+     *
+     * @param string $key the key to access the param
+     * @param mixed $default the default value to return case the key does not exit
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        if ($this->keyExists($key)) {
+            return $this->_params[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * @return array
+     **/
+    public function getAll()
+    {
+        return $this->_params;
+    }
+
+
+    /**
+     * Check if value is set
+     * @param string key
+     */
+    public function keyExists($key)
+    {
+        return array_key_exists($key, $this->_params);
+    }
+
+    /**
+     * Iterator implementation
+     */
+    public function key()
+    {
+        return key($this->_params);
+    }
+
+    /**
+     * Iterator implementation
+     */
+    public function rewind()
+    {
+        return reset($this->_params);
+    }
+
+    /**
+     * Iterator implementation
+     */
+    public function next()
+    {
+        return next($this->_params);
+    }
+
+    /**
+     * Iterator implementation
+     */
+    public function current()
+    {
+        return current($this->_params);
+    }
+
+    /**
+     * Iterator implementation
+     */
+    public function valid()
+    {
+        $key = key($this->_params);
+        return ($key !== null && $key !== false);
+    }
+
+    /**
+     * ArrayAccess implementation
+     */
+    public function offsetGet($key)
+    {
+        return $this->get($key);
+    }
+
+    /**
+     * ArrayAccess implementation
+     */
+    public function offsetSet($key, $value)
+    {
+        return $this->setValue($key, $value);
+    }
+
+    /**
+     * ArrayAccess implementation
+     */
+    public function offsetExists($key)
+    {
+        return $this->keyExists($key);
+    }
+
+    /**
+     * ArrayAccess implementation
+     */
+    public function offsetUnset($key)
+    {
+        if ($this->offsetExists($key)) {
+            unset($this->_params[$key]);
+        }
+
+        return $this;
+
+    }
+
+}
---
+++ tests/Gs/ParamsTest.php
@@ -0,0 +1,110 @@
+<?php
+
+require_once 'Gs/Params.php';
+
+
+/**
+ * @author Marcelo Jacobus <marcelo.jacobus@gmail.com>
+ */
+class Gs_ParamsTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Gs_Params
+     */
+    protected $o;
+
+
+    public function setUp()
+    {
+        $this->o = new Gs_Params(array(
+            'a' => 'Argentina',
+            'b' => 'Brazil',
+        ));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanGetAll()
+    {
+        $expectedParams = array(
+            'a' => 'Argentina',
+            'b' => 'Brazil',
+        );
+
+        $this->assertEquals($expectedParams, $this->o->getAll());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanGetParam()
+    {
+        $this->assertEquals('Argentina', $this->o->get('a'));
+        $this->assertEquals('Brazil', $this->o->get('b'));
+        $this->assertNull($this->o->get('c'));
+        $this->assertEquals('default', $this->o->get('c', 'default'));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanCheckIfValueIsSet()
+    {
+        $this->assertTrue($this->o->keyExists('a'));
+        $this->assertFalse($this->o->keyExists('c'));
+        $this->assertTrue($this->o->offsetExists('a'));
+        $this->assertFalse($this->o->offsetExists('c'));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanIterateObject()
+    {
+        $values = array();
+
+        foreach($this->o as $value) {
+            $values[] = $value;
+        }
+
+        $this->assertEquals(array('Argentina', 'Brazil'), $values);
+
+        $values = array();
+
+        foreach($this->o as $key => $value) {
+            $values[$key] = $value;
+        }
+
+        $this->assertEquals($this->o->getAll(), $values);
+    }
+
+    /**
+     * @test
+     */
+    public function itCanBeAccessedAsArray()
+    {
+        $this->assertEquals('Argentina', $this->o['a']);
+        $this->assertNull($this->o['c']);
+    }
+
+    /**
+     * @test
+     */
+    public function itCanSetValueAsArray()
+    {
+        $this->o['c'] = 'Cuba';
+        $this->assertEquals('Cuba', $this->o['c']);
+    }
+
+    /**
+     * @test
+     **/
+    public function canUnsetValue()
+    {
+        $this->o->offsetUnset('a');
+        $this->assertEquals(array('b' => 'Brazil'), $this->o->getAll());
+    }
+
+}
+
