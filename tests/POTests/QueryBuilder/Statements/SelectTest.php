d8ffd9e931dc4c5c929c31bf22076a081070c868
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-07-22 22:08:31 -0300

    Removed methods that test implementation detail

--- tests/POTests/QueryBuilder/Statements/SelectTest.php
+++ tests/POTests/QueryBuilder/Statements/SelectTest.php
@@ -22,42 +22,66 @@ class SelectTest extends PHPUnit_Framework_TestCase
 
     public function testGetSelect()
     {
-        $this->assertInstanceOf('PO\QueryBuilder\Clauses\SelectClause', $this->object->getSelect());
+        $this->assertInstanceOf(
+            'PO\QueryBuilder\Clauses\SelectClause',
+            $this->object->getSelect()
+        );
     }
 
     public function testGetFrom()
     {
-        $this->assertInstanceOf('PO\QueryBuilder\Clauses\FromClause', $this->object->getFrom());
+        $this->assertInstanceOf(
+            'PO\QueryBuilder\Clauses\FromClause',
+            $this->object->getFrom()
+        );
     }
 
     public function testGetWhere()
     {
-        $this->assertInstanceOf('PO\QueryBuilder\Clauses\WhereClause', $this->object->getWhere());
+        $this->assertInstanceOf(
+            'PO\QueryBuilder\Clauses\WhereClause',
+            $this->object->getWhere()
+        );
     }
 
     public function testGetOrder()
     {
-        $this->assertInstanceOf('PO\QueryBuilder\Clauses\OrderClause', $this->object->getOrder());
+        $this->assertInstanceOf(
+            'PO\QueryBuilder\Clauses\OrderClause',
+            $this->object->getOrder()
+        );
     }
 
     public function testGetLimit()
     {
-        $this->assertInstanceOf('PO\QueryBuilder\Clauses\LimitClause', $this->object->getLimit());
+        $this->assertInstanceOf(
+            'PO\QueryBuilder\Clauses\LimitClause',
+            $this->object->getLimit()
+        );
     }
 
     public function testGetJoins()
     {
-        $this->assertInstanceOf('PO\QueryBuilder\Clauses\JoinClause', $this->object->getJoins());
+        $this->assertInstanceOf(
+            'PO\QueryBuilder\Clauses\JoinClause',
+            $this->object->getJoins()
+        );
     }
 
     public function testGetGroup()
     {
-        $this->assertInstanceOf('PO\QueryBuilder\Clauses\GroupClause', $this->object->getGroup());
+        $this->assertInstanceOf(
+            'PO\QueryBuilder\Clauses\GroupClause',
+            $this->object->getGroup()
+        );
     }
 
     public function testGetHelper()
     {
-        $this->assertInstanceOf('PO\QueryBuilder\Helper', $this->object->getHelper());
+        $this->assertInstanceOf(
+            'PO\QueryBuilder\Helper',
+            $this->object->getHelper()
+        );
     }
 
     public function testItInitializesWithTheCorrectHelper()
@@ -66,23 +90,6 @@ class SelectTest extends PHPUnit_Framework_TestCase
         $options = array('helper' => $helper);
         $object = new QueryBuilder($options);
         $this->assertSame($helper, $object->getHelper());
-    }
-
-    public function testItCanSetAddFieldsToTheSelectClauseAsStringAndReturnBuilder()
-    {
-        $object = $this->object->select('field')->select(array('one', 'two'));
-        $this->assertSame($this->object, $object);
-        $this->assertEquals(array('field', 'one', 'two'), $this->object->getSelect()->getParams());
-    }
-
-    public function testCanSetFromAsStringAndReturnBuilder()
-    {
-        $object = $this->object->from('table');
-        $this->assertSame($this->object, $object);
-        $this->assertEquals(array('table'), $this->object->getFrom()->getParams());
-
-        $object = $this->object->from(array('table1', 'table2'));
-        $this->assertEquals(array('table1', 'table2'), $this->object->getFrom()->getParams());
     }
 
     public function testItCanConvertQueryToString()
@@ -140,61 +147,8 @@ class SelectTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($sql, $this->object->toSql($params));
     }
 
-    public function testItAddsInnerJoin()
-    {
-        $this->object->from('table')->innerJoin('t1')->innerJoin('t2', 't1.id = t2.t1_id');
-        $sql = 'INNER JOIN t1 INNER JOIN t2 ON t1.id = t2.t1_id';
-        $this->assertEquals($sql, $this->object->getJoins()->toSql());
-    }
-
-    public function testItAddsLeftJoin()
-    {
-        $this->object->from('table')->leftJoin('t1')->leftJoin('t3', 't1.id = t3.t1_id');
-        $sql = 'LEFT JOIN t1 LEFT JOIN t3 ON t1.id = t3.t1_id';
-        $this->assertEquals($sql, $this->object->getJoins()->toSql());
-    }
-
     public function testItCallsToSqlWhenConvertingToString()
     {
         $this->assertEquals($this->object->toSql(), (string) $this->object);
-    }
-
-    public function testItProvidesInterfaceForAddingConditions()
-    {
-        $this->object->where('a = 1')
-            ->where('a', 'b')
-            ->where('a', 'x', '!=')
-            ->where(array(
-                'bar' => 'foo',
-                'foobar' => 'foo'
-            ));
-
-        $expectedParams = array(
-            'a = 1',
-            'a = "b"',
-            'a != "x"',
-            'bar = "foo"',
-            'foobar = "foo"',
-        );
-
-        $this->assertEquals($expectedParams, $this->object->getWhere()->getParams());
-    }
-
-    public function testItProvidesInterfaceForAddingOrder()
-    {
-        $this->object->orderBy('a')->orderBy(array('b', 'c'));
-
-        $expectedParams = array( 'a', 'b', 'c');
-
-        $this->assertEquals($expectedParams, $this->object->getOrder()->getParams());
-    }
-
-    public function testItProvidesInterfaceForAddingGroup()
-    {
-        $this->object->groupBy('a')->groupBy(array('b', 'c'));
-
-        $expectedParams = array( 'a', 'b', 'c');
-
-        $this->assertEquals($expectedParams, $this->object->getGroup()->getParams());
     }
 }
